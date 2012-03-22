c28be1314ad8b7d79b1d9f0679420e26bd0e088c
samMScript <samuel.masuy@gmail.com>
2012-03-22 16:43:33 -0400

    first commit arduino code

---
+++ sensor_control.ino
@@ -0,0 +1,40 @@
+#include <Servo.h> 
+ 
+Servo benwoodServo;
+
+int fowardWheel = 2;
+int backwardWheel = 3;
+
+int motor = 6;
+
+void setup()
+{ 
+  pinMode(fowardWheel, OUTPUT);
+  pinMode(backwardWheel, OUTPUT);
+
+  benwoodServo.attach(10);
+}
+void foward()
+{  
+  digitalWrite(fowardWheel, HIGH);
+  digitalWrite(backwardWheel, LOW);
+  return;
+}
+void backward()
+{
+  digitalWrite(fowardWheel, LOW);
+  digitalWrite(backwardWheel, HIGH);
+  return;
+}
+void loop()
+{
+  foward();
+  analogWrite(motor, 200); 
+  benwoodServo.write(50);
+  delay(1000);
+  
+  backward();
+  analogWrite(motor, 200);
+  benwoodServo.write(120);
+  delay(1000);
+}
